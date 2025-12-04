8a2d7bc2dabc20e2ab8f62b70389172c7130776b
Kyle Smith <kyle.smith@rolemodelsoftware.com>
2025-12-03 20:47:56 -0600

    Fix database connection setup

--- app/models/books/application_record.rb
+++ app/models/books/application_record.rb
@@ -1,6 +1,5 @@
 module Books
-  class ApplicationRecord < ActiveRecord::Base
+  class ApplicationRecord < ApplicationRecord
     self.abstract_class = true
-    connects_to database: { writing: :books, reading: :books }
   end
 end
