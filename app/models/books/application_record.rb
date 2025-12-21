0afd611d76636a1b6c87ae6fb3afc7b93588b437
Kyle Smith <kyle.smith@rolemodelsoftware.com>
2025-12-20 21:29:26 -0600

    Set up isolated DB connection

--- app/models/books/application_record.rb
+++ app/models/books/application_record.rb
@@ -1,5 +1,9 @@
 module Books
   class ApplicationRecord < ApplicationRecord
     self.abstract_class = true
+
+    connects_to shards: {
+      books: { writing: :books, reading: :books }
+    }
   end
 end
