0d01a4f2a3d2f26fbe26c5e7950ea115c1136935
Kyle Smith <kyle.smith@rolemodelsoftware.com>
2025-12-26 10:20:32 -0600

    Add test for default book lists

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,5 +6,11 @@ module Books
       user = User.new(email_address: " DOWNCASED@EXAMPLE.COM ")
       assert_equal("downcased@example.com", user.email_address)
     end
+
+    test "sets up default book lists on create" do
+      user = User.new(email_address: "user@example.com", password: "password")
+      user.save!
+      assert_equal(user.book_lists.order(:name).pluck(:name), ["Read", "To Read"])
+    end
   end
 end
